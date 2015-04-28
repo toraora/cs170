fe5ec3e81ea950d828bc21440b552a64476790aa
nathanyw <imanasianninja@gmail.com>
2015-04-27 21:23:20 -0700

    now correctly limits choices based on number of each color remaining (a run of simulate_ant will never fail (hopefully))

--- nptsp.py
+++ nptsp.py
@@ -4,11 +4,10 @@ import math
 import random
 import multiprocessing as mp
 
-
 #import sys
 #data = sys.stdin.readlines()
 
-data = open('4.in', 'r').readlines()
+data = open('3.in', 'r').readlines()
 
 # read / parse data
 n = int(data[0])
@@ -20,7 +19,7 @@ if color[0] == 'B':
     color = ['B' if color[i] == 'R' else 'R' for i in range(n)]
 
 # algorithm parameters / variables (attractiveness already have power to alpha applied to it
-iterations = 500
+iterations = 200
 ants = 1000
 alpha = 2
 beta = 3
@@ -48,15 +47,13 @@ def max_choice(dictionary):
     return max(choices, key = lambda x: x[1])[0]
 
 def update_trail_levels(paths):
-    scaling_factor = (0. + ants) / len(paths)
-
     for i in range(n):
         for j in range(n):
             trail_level[i][j] *= (1-evap_const)
 
     for path in paths:
         for i in range(n-1):
-            trail_level[path[0][i]][path[0][i+1]] += (0. + Q) / path[1] * graph[path[0][i]][path[0][i+1]] * scaling_factor
+            trail_level[path[0][i]][path[0][i+1]] += (0. + Q) / path[1] * graph[path[0][i]][path[0][i+1]]
 
 def simulate_ant(choice_fn = random_choice):
     red = set(list(red_nodes))
@@ -66,26 +63,39 @@ def simulate_ant(choice_fn = random_choice):
     red.remove(0)
     path = [0]
     length = 0
+    num_red_prefix = 0
     while red_left + blue_left:
         cur_node = path[-1]
-        if len(path) >= 3:
-            if color[path[-1]] == color[path[-2]] == color[path[-3]]: # three nodes of same color, must switch
-                if color[path[-1]] == "R":
-                    choices = {node:attractiveness[cur_node][node]*trail_level[cur_node][node]**beta for node in blue}
+        if len(path) == 3: # count number of red nodes in a row at the beginning
+            for i in range(len(path)):
+                if color[path[i]] == 'R':
+                    num_red_prefix += 1
                 else:
-                    choices = {node:attractiveness[cur_node][node]*trail_level[cur_node][node]**beta for node in red}
+                    break
+        # make sure we have enough blue nodes left; this also avoid three in a row on the boundary
+        if blue_left == math.ceil((n - len(path) - 4 + num_red_prefix) / 3.):
+            choices = {node:attractiveness[cur_node][node]*trail_level[cur_node][node]**beta for node in red}
+        # make sure we have enough red nodes left
+        elif red_left == math.ceil((n - len(path) - 1) / 3.):
+            choices = {node:attractiveness[cur_node][node]*trail_level[cur_node][node]**beta for node in blue}
+        else:
+            if len(path) >= 3:
+                if color[path[-1]] == color[path[-2]] == color[path[-3]]: # three nodes of same color, must switch
+                    if color[path[-1]] == "R":
+                        choices = {node:attractiveness[cur_node][node]*trail_level[cur_node][node]**beta for node in blue}
+                    else:
+                        choices = {node:attractiveness[cur_node][node]*trail_level[cur_node][node]**beta for node in red}
+                else:
+                    r_dict = {node:attractiveness[cur_node][node]*trail_level[cur_node][node]**beta for node in red}
+                    b_dict = {node:attractiveness[cur_node][node]*trail_level[cur_node][node]**beta for node in blue}
+                    r_dict.update(b_dict)
+                    choices = r_dict
             else:
                 r_dict = {node:attractiveness[cur_node][node]*trail_level[cur_node][node]**beta for node in red}
                 b_dict = {node:attractiveness[cur_node][node]*trail_level[cur_node][node]**beta for node in blue}
                 r_dict.update(b_dict)
                 choices = r_dict
-        else:
-            r_dict = {node:attractiveness[cur_node][node]*trail_level[cur_node][node]**beta for node in red}
-            b_dict = {node:attractiveness[cur_node][node]*trail_level[cur_node][node]**beta for node in blue}
-            r_dict.update(b_dict)
-            choices = r_dict
-        if len(choices) == 0:
-            return "fail"
+        assert (len(choices) != 0), "ant failed to make path"
         choice = choice_fn(choices)
         path.append(choice)
         length += graph[path[-2]][path[-1]]
@@ -105,54 +115,9 @@ threadpool = mp.Pool(4)
 # main loop
 for i in range(iterations):
     print "iteration %i..." %i
-    paths = [] 
-    results = threadpool.map(simulate_ant, [random_choice] * ants)
-    for result in results:
-        if result == "fail":
-            continue;
-        paths.append(result)
+    paths = threadpool.map(simulate_ant, [random_choice] * ants)
     update_trail_levels(paths)
 
+# best path as determined by ACO
 print simulate_ant(choice_fn = max_choice)
 
-
-"""
-        red = set(list(red_nodes))
-        blue = set(list(blue_nodes))
-        red_left = (n+1)/2 - 1
-        blue_left = (n+1)/2
-        red.remove(0)
-        path = [0]
-        length = 0
-        while red_left + blue_left:
-            cur_node = path[-1]
-            if len(path) >= 3:
-                if color[path[-1]] == color[path[-2]] == color[path[-3]]: # three nodes of same color, must switch
-                    if color[path[-1]] == "R":
-                        choices = {node:attractiveness[cur_node][node]*trail_level[cur_node][node]**beta for node in blue}
-                    else:
-                        choices = {node:attractiveness[cur_node][node]*trail_level[cur_node][node]**beta for node in red}
-                else:
-                    r_dict = {node:attractiveness[cur_node][node]*trail_level[cur_node][node]**beta for node in red}
-                    b_dict = {node:attractiveness[cur_node][node]*trail_level[cur_node][node]**beta for node in blue}
-                    r_dict.update(b_dict)
-                    choices = r_dict
-            else:
-                r_dict = {node:attractiveness[cur_node][node]*trail_level[cur_node][node]**beta for node in red}
-                b_dict = {node:attractiveness[cur_node][node]*trail_level[cur_node][node]**beta for node in blue}
-                r_dict.update(b_dict)
-                choices = r_dict
-            choice = random_choice(choices)
-            path.append(choice)
-            length += graph[path[-2]][path[-1]]
-            if color[choice] == 'R':
-                red.remove(choice)
-                red_left -= 1
-            else:
-                blue.remove(choice)
-                blue_left -= 1
-        paths.append([path, length])
-    update_trail_levels(paths)
-
-
-"""
