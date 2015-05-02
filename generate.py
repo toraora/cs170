5a81382f41ac6996c608d9bac203944da1059a32
Nicholas Lin <nblin@berkeley.edu>
2015-05-02 03:39:04 -0700

    a

--- generate.py
+++ generate.py
@@ -1,9 +1,9 @@
 import random
 
-n = input('')
 a = [[0 for _ in range(50)] for _ in range(50)]
 
-path = [random.randint(30,45) for i in range(50)]
+lowest_path = [random.randint(30,45) for i in range(50)]
+"""
 def path(n):
     ret_path = [R]*25+[B]*25
     random.shuffle(ret_path)
@@ -27,23 +27,26 @@ def not_valid(path):
             if b >= 3:
                 return False
     return True
-
-valid_path = path(1)
+"""
+#valid_path = path(1)
 for i in range(50):
     for j in range(i+1,50):
         if i == j:
             a[i][j] = 0
         else:
-            a[i][j] = a[j][i] = random.randint(15,101)
+            a[i][j] = a[j][i] = random.randint(23,100)
 
 for i in range(50):
     j = (i+1)%50
-    a[i][j] = a[j][i] = path[i]
+    a[i][j] = a[j][i] = lowest_path[i]
+
+print 50
 
 for x in a:
     out = ''
     for y in x:
-        out += '%i' % y
+        out += '%i ' % y
     print out
 
-print ''.join(valid_path)
+print 'RB'*25
+#print ''.join(valid_path)
